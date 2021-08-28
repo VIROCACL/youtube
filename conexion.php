576229f37c91ca435a1d90e42c203915ee84716f
VICENTE RODRIGUEZ <89627152+VIROCACL@users.noreply.github.com>
2021-08-28 15:18:34 -0400

    Add files via upload

---
+++ conexion.php
@@ -0,0 +1,8 @@
+<?php 
+$con=mysqli_connect("localhost","root","","youtube");
+
+if (!$con) {
+	die("No se logro la conexion");
+}
+
+?>
---
+++ menu.php
@@ -0,0 +1,26 @@
+<nav class="navbar navbar-expand-lg navbar-dark"  style="background-color: #16072E  ; padding: 25px;" >
+ <!--  COLOR PAGINA  -->
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.php"><img src="imagenes/logo.png" width="80"></a>
+    <!--LOGO-->
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link " aria-current="page" href="#">Inicio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="quienessomos.php">Quienes somos</a>
+        </li>
+       <li class="nav-item">
+          <a class="nav-link" href="code.php">Codigo</a>
+        </li>
+      </ul>
+      <form class="d-flex" action="login.php">
+        <button class="btn btn-outline-light" type="submit">ACCESO LOGIN</button>
+      </form>
+    </div>
+  </div>
+</nav>
